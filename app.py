d9ae92b1b65e838d532de718122ef86a648a6324
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2022-08-29 14:12:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
-header = st.
-st.write("Hello World!")
+header = st.beta_container()
+dataset = st.beta_container()
+
+with header:
+    st.title("Analysis of Nigerian Food Prices 2022")
+
+#st.write("Hello World!")
 
