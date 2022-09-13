c16d3e3a7f291f55e2413f47d00626869502d82a
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2022-09-13 15:54:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+#import matplotlib.pyplot as plt
 import pandas as pd
-import matplotlib.pyplot as plt
+
 
 
 header = st.beta_container()
@@ -19,26 +20,26 @@ low_high = pd.read_csv("lowest_highest.csv")
 st.write(price_data.head())
 st.write(low_high.head())
 
-# defining variables
-# high = "Highest price is from " + low_high["Highest"]
-# low = "Lowest price is from " + low_high["Lowest"]
-# max = price_data["Beef Bone in"].max()
-# min = price_data["Beef Bone in"].min()
-# price = price_data["Beef Bone in"].tail(1)[0]
-# y = (max - min) / price
+    #defining variables
+high = "Highest price is from " + low_high["Highest"]
+low = "Lowest price is from " + low_high["Lowest"]
+max = price_data["Beef Bone in"].max()
+min = price_data["Beef Bone in"].min()
+price = price_data["Beef Bone in"].tail(1)[0]
+y = (max - min) / price
 
-# # plot graph
-fig, ax = plt.subplots(figsize=(15,10))
-# ax.plot(price_data['Beef Bone in'])
-# plt.ylabel("Price in Naira (₦)", fontsize=15)
-# plt.xlabel("Year", fontsize=15)
-# fig.text(0.15, 0.85, "Beef Bone in (Jan 2017 - July 2022)", fontsize=18)
-# fig.text(0.67, 0.17, high[4], fontsize=13)
-# fig.text(0.67, 0.20, low[4], fontsize=13)
-# fig.text(0.85, y, "₦ "+ str(price))
-# for s in ['top', 'right']:
-#     ax.spines[s].set_visible(False)
-# # Show plot
-st.pyplot(fig)
+    # # plot graph
+    # fig, ax = plt.subplots(figsize=(15,10))
+    # ax.plot(price_data['Beef Bone in'])
+    # plt.ylabel("Price in Naira (₦)", fontsize=15)
+    # plt.xlabel("Year", fontsize=15)
+    # fig.text(0.15, 0.85, "Beef Bone in (Jan 2017 - July 2022)", fontsize=18)
+    # fig.text(0.67, 0.17, high[4], fontsize=13)
+    # fig.text(0.67, 0.20, low[4], fontsize=13)
+    # fig.text(0.85, y, "₦ "+ str(price))
+    # for s in ['top', 'right']:
+    #     ax.spines[s].set_visible(False)
+    # # Show plot
+    # st.pyplot(fig)
 
 
