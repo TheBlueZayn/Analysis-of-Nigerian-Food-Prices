65a37f97ae86ce96c3866eeb0fc21485ab488193
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2022-09-13 14:58:12 +0100

    edit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 header = st.beta_container()
 dataset = st.beta_container()
@@ -7,7 +8,10 @@ with header:
     st.title("""Analysis of Nigerian Food Prices 2022
                 By Zaynab Arowosegbe""")
     st.text("More Info on the analysis")
+
 with dataset:
     st.header("About the dataset")
+price_data = pd.read_csv("prices.csv")
+st.write(price_data.head())
 
 
