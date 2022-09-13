d59b0ad1e376db90d7cb79aef9ad7956ee40dfbe
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2022-09-13 16:11:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,40 +14,18 @@ with header:
 with dataset:
     st.header("About the dataset")
 
-# Load datasets
+    price_data = pd.read_csv("prices.csv")
+    price_data["Date"] = pd.to_datetime(price_data["Date"])
+    low_high = pd.read_csv("lowest_highest.csv")
+    st.write(price_data.head())
+    st.write(low_high.head())
 
-price_data = pd.read_csv("prices.csv")
-price_data["Date"] = pd.to_datetime(price_data["Date"])
-low_high = pd.read_csv("lowest_highest.csv")
-st.write(price_data.head())
-st.write(low_high.head())
+    line = pd.DataFrame(price_data["Beef Bone in"].value_counts()).head()
+    st.bar_chart(line)
 
 
 
 
-#defining variables
-with header:
-    st.text("hcudhchd")
-#def line_plot():
-    high = "Highest price is from " + low_high["Highest"]
-    low = "Lowest price is from " + low_high["Lowest"]
-    max = price_data["Beef Bone in"].max()
-    min = price_data["Beef Bone in"].min()
-    price = price_data["Beef Bone in"].tail(1)[0]
-    y = (max - min) / price
-    
-    # # plot graph
-    # fig, ax = plt.subplots(figsize=(15,10))
-    # ax.plot(price_data['Beef Bone in'])
-    # plt.ylabel("Price in Naira (₦)", fontsize=15)
-    # plt.xlabel("Year", fontsize=15)
-    # fig.text(0.15, 0.85, "Beef Bone in (Jan 2017 - July 2022)", fontsize=18)
-    # fig.text(0.67, 0.17, high[4], fontsize=13)
-    # fig.text(0.67, 0.20, low[4], fontsize=13)
-    # fig.text(0.85, y, "₦ "+ str(price))
-    # for s in ['top', 'right']:
-    #     ax.spines[s].set_visible(False)
-    # # Show plot
-    # st.pyplot()
+
 
 
