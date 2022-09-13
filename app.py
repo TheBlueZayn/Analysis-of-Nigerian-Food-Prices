1cf9c4c75c3dff04b8b8a8c0bcb6a7f7eb31e49a
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2022-09-13 16:51:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,10 @@ with dataset:
     # Load datasets
     price_data = pd.read_csv("prices.csv")
     price_data["Date"] = pd.to_datetime(price_data["Date"])
-    low_high = pd.read_csv("lowest_highest.csv")
-    st.write(price_data.head())
     price_data.set_index("Date", inplace=True)
+    low_high = pd.read_csv("lowest_highest.csv")
+    # Show data
+    st.write(price_data.head())
     st.write(low_high.head())
 
     
